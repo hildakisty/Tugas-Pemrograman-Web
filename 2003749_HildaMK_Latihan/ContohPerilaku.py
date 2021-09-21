053618fb8410a56ec89dae9aca03b5b096bd6377
hildakisty <90302483+hildakisty@users.noreply.github.com>
2021-09-21 13:13:21 +0700

    Add files via upload

---
+++ 2003749_HildaMK_Latihan/ContohPerilaku.py
@@ -0,0 +1,20 @@
+#Contoh Perilaku
+# buat kelasnya terlebih dahulu 
+class Mahasiswa:   
+    nama = None   
+    asal = None 
+
+    def perkenalan (self):     
+         print(f'Perkenalkan saya {self.nama} dari {self.asal}') 
+
+deni = Mahasiswa() 
+deni.nama = "Deni" 
+deni.asal = "Jawa Timur" 
+
+lendis = Mahasiswa() 
+lendis.nama = "Lendis Fabri" 
+lendis.asal = "Jawa Timur" 
+
+# panggil fungsi perkenalan 
+deni.perkenalan() 
+lendis.perkenalan() 
---
+++ 2003749_HildaMK_Latihan/DokumentasiKelas.py
@@ -0,0 +1,4 @@
+#Contoh Dokumentasi Kelas
+class Mahasiswa:   """   Kelas ini digunakan untuk mendefinisikan   objek Mahasiswa di kehidupan nyata   """
+
+print(Mahasiswa.__doc__) 
